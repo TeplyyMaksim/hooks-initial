6babfebc021b623337034f07f7073707213ea830
Maksym <teplyy.maksim@gmail.com>
2019-06-01 14:39:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ResourceList from 'ResourceList';
+import UserList from 'UserList';
 
-function App() {
+const App = () => {
+  const [resource, setResource] = useState('posts')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserList />
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
     </div>
   );
 }
